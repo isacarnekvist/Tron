338e4e29cdfe58d85ea4c34342ccc611654e8f6b
Isac Arnekvist <isac.arnekvist@gmail.com>
2014-05-05 17:08:28 +0200

    Attempt to check for texture intersect, not working properly yet...

---
+++ src/game/Geometry.java
@@ -0,0 +1,45 @@
+package game;
+
+import org.ejml.simple.SimpleMatrix;
+
+public class Geometry {
+	
+	private static final int X = 0;
+	private static final int Y = 1;
+	
+	/**
+	 * Checks too see if two lines intersect
+	 * @param l1p1 Point 1 coordinates of line 1
+	 * @param l1p2 Point 2 coordinates of line 1
+	 * @param l2p1 Point 1 coordinates of line 2
+	 * @param l2p2 Point 2 coordinates of line 2
+	 * @return true if intersect, false otherwise
+	 */
+	public static boolean linesIntersect(SimpleMatrix l1p1, SimpleMatrix l1p2, 
+										SimpleMatrix l2p1, SimpleMatrix l2p2) {
+		
+		SimpleMatrix A = new SimpleMatrix(2, 2, true, l1p2.get(Y) - l1p1.get(Y),
+													  l1p1.get(X) - l1p2.get(X),
+													  l2p2.get(Y) - l2p1.get(Y),
+													  l2p1.get(X) - l2p2.get(X));
+		SimpleMatrix B = new SimpleMatrix(2, 1, true, l1p1.get(X)*l1p2.get(Y) - l1p2.get(X)*l1p1.get(Y),
+													  l2p1.get(X)*l2p2.get(Y) - l2p2.get(X)*l2p1.get(Y));
+		
+		if(A.determinant() == 0) {
+			return false;
+		}
+		
+		SimpleMatrix XM = A.solve(B);
+		
+		if (XM.get(X) < Math.max(l1p1.get(X), l1p2.get(X))) {
+			if(XM.get(X) > Math.min(l1p1.get(X), l1p2.get(X))) {
+				if(XM.get(Y) < Math.max(l1p1.get(Y), l1p2.get(Y))) {
+					if(XM.get(Y) > Math.min(l1p1.get(Y), l1p2.get(Y))) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
